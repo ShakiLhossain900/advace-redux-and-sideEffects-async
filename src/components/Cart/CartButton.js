7f24743c4cba2159c6e98fc5ca5dcae953eca4b0
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-30 16:49:01 +0600

    adding cart redux toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  
+  const toggleCarthandler = () => {
+   
+    dispatch(uiActions.toggle())
+    
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCarthandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
