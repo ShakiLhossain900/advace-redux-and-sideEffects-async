f19df322d84ae4e0c0b603a0206c87c8f38d84f4
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-02 23:18:33 +0600

    adding using effect to the redux

--- src/App.js
+++ src/App.js
@@ -29,4 +29,4 @@ export default App;
 //complete using effect to the redux 
 //10,11,12,13 need to complete before 3 pm 03 octeber
 //tomorrow i will be complete this reducx module 
-// than next module i need to run for it
+// than next module i need to run for it 20 building a multi page SPA with react router
