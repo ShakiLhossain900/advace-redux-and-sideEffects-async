ff1fc2208119fd4b9b4915cabaaa574e8cc1ae2b
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-02 21:41:14 +0600

    adding the use effect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -8,6 +9,9 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart)
 
+useEffect(() => {
+
+},[]);
 
   return (
     <Layout>
