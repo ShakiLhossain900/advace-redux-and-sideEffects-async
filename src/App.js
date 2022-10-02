769c3a432f3e746395063c2541ded287fe2e025e
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-02 21:46:06 +0600

    adding the use effect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.cart)
+  const cart = useSelector((state) => state.cart);
 
-useEffect(() => {
-
-},[]);
+  useEffect(() => {
+    fetch("https://redux-project-4cdaa-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]); //whenever cart changes the data will be re-execute by using the cart dependency
 
   return (
     <Layout>
@@ -23,4 +26,4 @@ useEffect(() => {
 
 export default App;
 
-//done the project about redux toolkit 
+
