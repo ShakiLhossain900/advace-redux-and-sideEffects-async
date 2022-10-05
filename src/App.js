3dd002fbee15777ae70486818f5dcb0e3fcf6316
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-05 10:22:56 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -12,21 +12,22 @@ function App() {
   useEffect(() => {
     const sendCartData = async () => {
       const response = await fetch(
-        "https://redux-5703a-default-rtdb.firebaseio.com/cart.json",
+        "https://redux-practice-10cc2-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
           body: JSON.stringify(cart),
         }
       );
-       
-      if(!response.ok) {
-        throw new Error('Sending cart data Failed')
+
+      if (!response.ok) {
+        throw new Error("Sending cart data Failed");
       }
 
       const responseData = await response.json();
+
+      
     };
   }, [cart]); //whenever cart changes the data will be re-execute by using the cart dependency
-
   return (
     <Layout>
       {showCart && <Cart />}
