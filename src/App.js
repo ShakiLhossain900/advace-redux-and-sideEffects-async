8564a1bb672f0432f673de9ab838d18112339e56
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-05 10:58:34 +0600

    done working with app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -7,20 +7,20 @@ import Products from "./components/Shop/Products";
 import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
 
-let isInitail = true;
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-  
+
   useEffect(() => {
     const sendCartData = async () => {
       dispatch(
         uiActions.showNotification({
           status: "pending",
-          title: "sending..... ",
+          title: "Sending...",
           message: "Sending cart data!",
         })
       );
@@ -33,43 +33,43 @@ function App() {
       );
 
       if (!response.ok) {
-        throw new Error("Sending cart data Failed");
+        throw new Error("Sending cart data failed.");
       }
 
       dispatch(
         uiActions.showNotification({
           status: "success",
-          title: "success!",
+          title: "Success!",
           message: "Sent cart data successfully!",
         })
       );
     };
-    
-    //for amar notification ta age theke show kore tai ami ata soriye neoar jonnu ata koreci
-    if(isInitail){
-      isInitail = false;
+
+    if (isInitial) {
+      isInitial = false;
       return;
-    };
+    }
 
     sendCartData().catch((error) => {
       dispatch(
         uiActions.showNotification({
           status: "error",
-          title: "Error! ",
+          title: "Error!",
           message: "Sending cart data failed!",
         })
       );
     });
   }, [cart, dispatch]);
-  // here dispatch will never change only cart will be change
-  //cart here not only dependensy here now dispatch also dependensy here //whenever cart changes the data will be re-execute by using the cart dependency
+
   return (
     <Fragment>
-      {notification && <Notification
-      state={notification.status}
-      title={notification.title}
-      message={notification.message}
-      />}
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Layout>
         {showCart && <Cart />}
         <Products />
@@ -79,29 +79,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//the changing plan
-//3 to 5 i need to sleep
-//5 to 7 need to doing the code and the 7 to 10 i need to take preparation for exam
-//10 to 5  i will do code
-//than 9 am to 1 pm study than 2 to 5 again preparation (code 5 to 7 pm ) than 7 to 10 preparation for exam than 10 to 5 code agian
